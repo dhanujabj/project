79ae4c56458fc4d8ec86e422f930cc16ed119075
dhanujabj <132563333+dhanujabj@users.noreply.github.com>
2023-05-13 14:40:07 -0700

    image

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,7 +7,7 @@ const Login = () => {
     <div>
         <div className='tb'>
         <div className='tt'>
-         {/* <h1 className='ll'>Sign in</h1>  */}
+         <h1 className='ll'>Sign in</h1>  
         <TextField className='lb' id="oraphyutlined-basic" label="username" variant="outlined" /><br></br>
         <TextField className='sb' id="outlined-basic" label="password" variant="outlined" /><br></br>
          <Button className='id' variant='text'>forgot password?</Button>
