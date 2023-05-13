097683b6923b9d131957de5c986fbfb73ad22bc9
dhanujabj <132563333+dhanujabj@users.noreply.github.com>
2023-05-13 13:59:33 -0700

    space

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,11 +6,8 @@ const Login = () => {
   return (
     <div>
         <div className='tb'>
-       
         <div className='tt'>
-         
          {/* <h1 className='ll'>Sign in</h1>  */}
-       
         <TextField className='lb' id="oraphyutlined-basic" label="username" variant="outlined" /><br></br>
         <TextField className='sb' id="outlined-basic" label="password" variant="outlined" /><br></br>
          <Button className='id' variant='text'>forgot password?</Button>
